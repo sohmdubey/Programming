5762e4a4b744f86e32bb7d085c269ff42922be87
Sohm Dubey <sd27295@pausd.us>
2017-08-03 14:31:45 -0700

    Updated MultiplayerFPS 1.0

---
+++ Unity/MultiplayerFPS/Assets/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+	private float speed = 5f;
+	[SerializeField]
+	private float lookSensitivity = 3f;
+
+	private PlayerMotor motor;
+
+	void Start()
+	{
+		motor = GetComponent<PlayerMotor> ();
+	}
+
+	void Update ()
+	{
+		float _Xmov = Input.GetAxisRaw ("Horizontal");
+		float _ZMov = Input.GetAxisRaw ("Vertical");
+
+		Vector3 _movHorizontal = transform.right * _Xmov;
+		Vector3 _movVertical = transform.forward * _ZMov;
+
+		Vector3 _velocity = (_movHorizontal + _movVertical).normalized * speed;
+
+		motor.Move (_velocity); 
+
+		float _yRot = Input.GetAxisRaw ("Mouse X");
+
+		Vector3 _rotation = new Vector3 (0f, _yRot, 0f) * lookSensitivity;
+
+		motor.Rotate (_rotation);
+
+		float _xRot = Input.GetAxisRaw ("Mouse Y");
+
+		Vector3 _cameraRotation = new Vector3 (_xRot, 0f, 0f) * lookSensitivity;
+
+		motor.RotateCamera (_cameraRotation);
+	}
+}
