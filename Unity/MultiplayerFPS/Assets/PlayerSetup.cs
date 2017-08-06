fdc4b9484ce76755efdecdd6c494e058975b5431
Sohm Dubey <sd27295@pausd.us>
2017-08-06 16:16:08 -0700

    Updated MultiplayerFPS 1.1

--- Unity/MultiplayerFPS/Assets/PlayerSetup.cs
+++ Unity/MultiplayerFPS/Assets/PlayerSetup.cs
@@ -1,7 +1,24 @@
 ﻿using UnityEngine;
+using UnityEngine.Networking;
 
-public class PlayerSetup : MonoBehaviour {
+public class PlayerSetup : NetworkBehaviour {
 
+    [SerializeField]
+    Behaviour[] componentsToDisable;
 
+    void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            for (int i = 0; i < componentsToDisable.Length; ++i)
+            {
+                componentsToDisable[i].enabled = false;
+            }
+        } else
+        {
+            Camera.main.gameObject.SetActive(false);
+        }
+    }
 
 }
+ 
